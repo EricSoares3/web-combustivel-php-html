d4e84f88ff8f7cfeb4806998bc4f29f5ab3f4496
Eric Soares <contato.eric.sm@gmail.com>
2018-10-14 23:55:50 -0300

    Adição do condicional IF

--- calculo.php
+++ calculo.php
@@ -1,16 +1,22 @@
 <?php
+if ($_POST) {
+    $distancia = $_POST['distancia'];
+    $autonomia = $_POST['autonomia'];
 
-$distancia = $_POST['distancia'];
-$autonomia = $_POST['autonomia'];
+    $valorGasolina = 4.80;
+    $valorDiesel = 3.80;
+    $valorAlcool = 3.90;
 
-$valorGasolina = 4.80;
-$valorDiesel = 3.80;
-$valorAlcool = 3.90;
+    $calculoGasolina = ( $distancia / $autonomia ) * $valorGasolina;
+    $calculoDiesel = ( $distancia / $autonomia ) * $valorDiesel;
+    $calculoAlcool = ( $distancia / $autonomia ) * $valorAlcool;
 
-$calculoGasolina = ( $distancia / $autonomia ) * $valorGasolina;
-$calculoDiesel = ( $distancia / $autonomia ) * $valorDiesel;
-$calculoAlcool = ( $distancia / $autonomia ) * $valorAlcool;
+    echo "<p> O valor do consume de Gasolina é R$ " .$calculoGasolina ."</p>";
+    echo "<p> O valor do consume de Alcool é R$ " .$calculoAlcool ."</p>";
+    echo "<p> O valor do consume de Diesel é R$ " .$calculoDiesel ."</p>";
 
-echo "<p> O valor do consume de Gasolina é R$ " .$calculoGasolina ."</p>";
-echo "<p> O valor do consume de Alcool é R$ " .$calculoAlcool ."</p>";
-echo "<p> O valor do consume de Diesel é R$ " .$calculoDiesel ."</p>";
+    echo "<p>Valor da distâcia é: " .$distancia  ."</p>";
+    echo "<p>Valor da autonomia é: " .$autonomia  ."</p>";
+} else {
+    echo "Nenhum dado recebido do Fórmulario.";
+}
