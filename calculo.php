0a4c31eb1f5c6555c2882cde66db69b5935978b0
Eric Soares <contato.eric.sm@gmail.com>
2018-10-15 00:28:08 -0300

    Adicionando acabemento visual

--- calculo.php
+++ calculo.php
@@ -1,4 +1,6 @@
 <?php
+$mensagem = "";
+
 if ($_POST) {
     $distancia = $_POST['distancia'];
     $autonomia = $_POST['autonomia'];
@@ -20,9 +22,9 @@ if ($_POST) {
             $calculoAlcool = ( $distancia / $autonomia ) * $valorAlcool;
             $calculoAlcool = number_format($calculoAlcool, 2, ',', '.');
 
-            echo "<p> O valor do consume de Gasolina é R$ " .$calculoGasolina ."</p>";
-            echo "<p> O valor do consume de Alcool é R$ " .$calculoAlcool ."</p>";
-            echo "<p> O valor do consume de Diesel é R$ " .$calculoDiesel ."</p>";
+            $mensagem.= "<p> O valor do consume de Gasolina é R$ " .$calculoGasolina ."</p>";
+            $mensagem.= "<p> O valor do consume de Alcool é R$ " .$calculoAlcool ."</p>";
+            $mensagem.= "<p> O valor do consume de Diesel é R$ " .$calculoDiesel ."</p>";
 
         }else {
             echo "O valor da distância e da autonomia devem ser maior que Zero! ";
@@ -33,3 +35,27 @@ if ($_POST) {
 } else {
     echo "Nenhum dado recebido do Fórmulario.";
 }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <title>Calculo de Consumo de Combustível</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">   
+</head>
+<body>  
+    <main>
+        <div class="painel">
+            <h2>Resultado do cálculo de consumo</h2>
+            <div class="conteudo-painel"> 
+                <?php
+                echo $mensagem;
+                ?>
+                <a class="botao" href="index.php">Voltar</a>
+            </div>            
+        </div>  
+    </main>  
+</body>
+</html>
